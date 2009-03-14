2477e43742d401115c665589515a510b51b99b59
daniel@d15.biz <daniel@d15.biz@a20db550-102d-11de-b0c5-ff504ef5068a>
2009-03-14 03:51:20 +0000

    Slowly getting there...

---
+++ Run_DoubleTap/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using PluginInterface;
+
+namespace Run_DoubleTap
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			PluginServices ps = new PluginServices();
+			ps.Load("DoubleTap");
+		}
+	}
+}
